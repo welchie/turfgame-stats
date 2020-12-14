8c2022efc2da80159b32fd7b67ad657d1a278fa3
welchie <chris@weewelchie.org>
2020-12-14 01:29:38 +0000

    Updated for feeds URL

--- src/main/java/org/weewelchie/turfgame/TurfDataRestController.java
+++ src/main/java/org/weewelchie/turfgame/TurfDataRestController.java
@@ -10,19 +10,22 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
-import org.weewelchie.turfgame.rest.client.UserData;
 import org.weewelchie.turfgame.rest.client.User;
+import org.weewelchie.turfgame.rest.client.UserData;
 
 @Path("/turfgame")
 public class TurfDataRestController {
 
     private static Logger LOGGER = Logger.getLogger(TurfDataRestController.class.getName());
+    private static String BASE_URL = "https://api.turfgame.com/";
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -44,7 +47,7 @@ public class TurfDataRestController {
 
         LOGGER.info("JSON: " + Entity.json(user));
         Client client = ClientBuilder.newClient();
-        WebTarget target = client.target("https://api.turfgame.com/v4/users/");
+        WebTarget target = client.target(BASE_URL + "/v4/users/");
         String response = target.request(MediaType.APPLICATION_JSON).accept(MediaType.TEXT_PLAIN_TYPE)
                 .post(Entity.json(user.toJSON()), String.class);
 
@@ -55,8 +58,7 @@ public class TurfDataRestController {
             turfData = new ObjectMapper().readValue(resp, UserData.class);
 
         } catch (JsonProcessingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            LOGGER.severe("JsonProcessingException: " + e.getMessage());
         }
         
         LOGGER.info("Retrieved User data: " + turfData);
@@ -64,5 +66,26 @@ public class TurfDataRestController {
         return turfData;
 
 
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/feeds")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String getFeeds() {
+
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(BASE_URL + "/v4/feeds");
+        Builder request = target.request(MediaType.APPLICATION_JSON);
+        Response response = request.buildGet().invoke();
+        //Response response = target.request(MediaType.APPLICATION_JSON).accept(MediaType.TEXT_PLAIN_TYPE).get();
+        LOGGER.info("Response Status: " + response.getStatus());
+        LOGGER.info("Response: " + response.getEntity().toString());
+
+        LOGGER.info("EntrySet: " + response.getMetadata().entrySet().toString());
+
+        LOGGER.info("Entity JSON: " + Entity.json(response.getEntity()).toString());
+
+        return response.toString();
     }
 }
